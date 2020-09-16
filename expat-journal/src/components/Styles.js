cb152430ae01a09149af970646c91a293ec84615
Raul Flores <raulefloresjr@gmail.com>
2020-09-15 20:28:38 -0700

    fixed import issue in index.js

---
+++ expat-journal/src/components/Styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core"; // import makeStyles from @material-ui/core
+
+const useStyles = makeStyles({
+	//similar to CSS
+	// name of class: {
+	//    styles go here
+	//    key: 'value'
+	//}
+	//eg:
+	app: {
+		color: "red",
+	},
+});
+
+export default useStyles;
